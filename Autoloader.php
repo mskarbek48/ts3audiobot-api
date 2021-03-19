24ecdbaa8f9499eb9a08ff3911b6bd98a1db30ed
Lukieer <macieqskarbek@gmail.com>
2021-03-19 13:58:45 +0100

    develop

---
+++ Autoloader.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * This file is apart of the ts3audiobot-api project.
+ *
+ * Copyright 		:: (C) 2021 Maciej Skarbek
+ * Website			:: mskarbek.pl
+ * Github			:: https://github.com/Lukieer/ts3audiobot-api
+ * 
+ * Created			:: 23 November 2020	
+ * last modified	:: 17 March 2021
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+require_once('Request/Request.php');
+require_once('Instance/Instance.php');
+require_once('Bot/bot.php');
+
+use Lukieer\TS3AudioBot\Api;
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # ts3audiobot-api
+During work
+
+
+# Examples 
+
+* Play youtube url on bot id 15
+
+```
+<?php
+
+require_once('TS3AudioBot/Autoloader.php');
+
+
+$ts3ab = new Lukieer\TS3AudioBot\Api\Instance('51.43.53.56', 'login', 'password');
+
+$test = $ts3ab->bot(15)->play('https://www.youtube.com/watch?v=nZEbCkXJV1o');
+
+print_r($test);
+
+```
