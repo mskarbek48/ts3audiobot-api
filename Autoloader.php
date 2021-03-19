2bdf7e754f818bd65111f46b86d9e4bf47222784
mskarbek <66939603+Lukieer@users.noreply.github.com>
2021-03-19 12:19:22 +0100

    Add files via upload

---
+++ Autoloader.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once('Request/Request.php');
+require_once('Instance/Instance.php');
+require_once('Bot/bot.php');
+
+use Lukieer\TS3AudioBot\Api\Bot;
+use Lukieer\TS3AudioBot\Api\Instance;
+use Lukieer\TS3AudioBot\Api\Request;
